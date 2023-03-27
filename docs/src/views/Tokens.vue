4490347232a7ca6a93553f157dcccb8cfe9c940a
David Schwarz <david@ists-wurscht.at>
2023-03-27 21:53:12 +0200

    Prism fixed

--- docs/src/views/Tokens.vue
+++ docs/src/views/Tokens.vue
@@ -1,29 +1,19 @@
-<script>
-import { ref } from "vue";
-import { useTypescalesStore } from '../stores/typescales';
-// import Prism Editor
-import { PrismEditor } from 'vue-prism-editor';
-import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
+<script setup>
+  import { computed } from 'vue'
+  import { useTypescalesStore } from '../stores/typescales'
+  import { PrismEditor } from 'vue-prism-editor'
+  import 'vue-prism-editor/dist/prismeditor.min.css'
+  import { highlight, languages } from 'prismjs/components/prism-core'
+  import 'prismjs/components/prism-clike'
+  import 'prismjs/components/prism-javascript'
+  import 'prismjs/themes/prism-tomorrow.css'
 
-// import highlighting library (you can use any library you want just return html string)
-import { highlight, languages } from 'prismjs/components/prism-core';
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
+  const typescales = useTypescalesStore()
+  const code = computed(() => JSON.stringify(typescales.designTokens['font-scale'], null, 2))
 
-const typescales = useTypescalesStore();
-
-export default {
-  components: {
-    PrismEditor,
-  },
-  data: () => ({ code: JSON.stringify(typescales.designTokens['font-scale'], null, 2) }),
-  methods: {
-    highlighter(code) {
-      return highlight(code, languages.js); // languages.<insert language> to return html with markup
-    },
-  },
-};
+  function highlighter (code) {
+    return highlight(code, languages.js) // languages.<insert language> to return html with markup
+  }
 </script>
 
 <template>
@@ -32,21 +22,21 @@ export default {
 
 
 <style>
-/* required class */
-.my-editor {
-  /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
-  background: #2d2d2d;
-  color: #ccc;
+  /* required class */
+  .my-editor {
+    /* we dont use `language-` classes anymore so thats why we need to add background and text color manually */
+    background: #2d2d2d;
+    color: #ccc;
 
-  /* you must provide font-family font-size line-height. Example: */
-  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
-  font-size: 14px;
-  line-height: 1.5;
-  padding: 5px;
-}
+    /* you must provide font-family font-size line-height. Example: */
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+  }
 
-/* optional class for removing the outline */
-.prism-editor__textarea:focus {
-  outline: none;
-}
+  /* optional class for removing the outline */
+  .prism-editor__textarea:focus {
+    outline: none;
+  }
 </style>
