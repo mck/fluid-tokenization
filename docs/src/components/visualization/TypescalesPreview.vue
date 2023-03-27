5991ef9a1b0608d7ffb6f3723acfbd8ffa576ece
Marco Christian Krenn <marco.krenn@gmail.com>
2023-03-27 21:42:51 +0200

    add font-size style

--- docs/src/components/visualization/TypescalesPreview.vue
+++ docs/src/components/visualization/TypescalesPreview.vue
@@ -5,18 +5,18 @@ const typescales = useTypescalesStore();
 </script>
 
 <template>
-	<table class="table">
+	<table class="table w-full">
 		<thead>
 			<tr>
-				<th>Step</th>
-				<th v-for="item in typescales.typescales[0].slice(1, -1)" class="text-right">{{ item.breakpoint }}px</th>
+				<th class="text-left">Step</th>
+				<th v-for="item in typescales.typescales[0].slice(1, -1)" class="text-left">{{ item.breakpoint }}px</th>
 			</tr>
 		</thead>
 		<tbody>
 			<tr v-for="scale in typescales.typescales" class="align-bottom" :class="scale[0].step">
 				<td>{{ scale[0].step }}</td>
 				<td v-for="item in scale.slice(1, -1)"
-					:style="[{ 'line-height': '1em' }, { 'font-size': item.cssValue.toFixed(2) + 'px' }]">Aa ({{ item.cssValue.toFixed(0) }}px)</td>
+            :style="[{ 'line-height': '1em' }, { 'font-size': item.cssValue.toFixed(2) + 'px' }]">Aa <span class="font-size-info">({{ item.cssValue.toFixed(0) }}px)</span></td>
 			</tr>
 		</tbody>
 	</table>
